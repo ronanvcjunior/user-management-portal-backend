5b11cb2d6c2c2e442279a2ae25830ba4159a2970
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2021-12-09 17:44:47 -0300

    domain user

---
+++ src/main/java/com/ronan/usermanagementportal/domain/User.java
@@ -0,0 +1,60 @@
+package com.ronan.usermanagementportal.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "last_login_date")
+    private Date lastLoginDate;
+
+    @Column(name = "last_login_date_display")
+    private Date lastLoginDateDisplay;
+
+    @Column(name = "join_date")
+    private Date joinDate;
+
+
+    private String[] roles; // ROLE_USER{ read, edit }, ROLE_ADMIN{ delete }
+    private String[] authorities;
+    
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "is_not_locked")
+    private boolean isNotLocked;
+} 
